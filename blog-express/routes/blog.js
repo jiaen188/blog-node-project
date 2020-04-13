b07adad0b2a92a080259996b902c09a16b6622d1
gulifan <gulifan@dxy.cn>
2020-04-13 11:45:43 +0800

    feat: express初始化环境

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { 
+  getList,
+  getDetail,
+  newBlog,
+  upateBlog,
+  delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 router.get('/list', function(req, res, next) {
-  res.json({
-    errno: 0,
-    data: [1, 2, 3]
+  let { author = '', keyword = '' } = req.query
+
+  // if (req.query.isadmin) {
+  //   // 管理员界面
+  //   const loginCheckResult = loginCheck(req)
+  //   if (loginCheckResult) {
+  //     // 未登录
+  //     return loginCheckResult
+  //   }
+  //   // 强制查询自己的博客
+  //   author = req.session.username
+  // }
+
+  const result = getList(author, keyword)
+  return result.then(listData => {
+    res.json(
+      new SuccessModel(listData)
+    )
   })
 });
 
