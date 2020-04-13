88d4c45bfb0b9dbf7c4bd979b3a79eb1d9d628b4
gulifan <gulifan@dxy.cn>
2020-04-13 14:40:46 +0800

    feat: session连接redis ,登录中间件

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -12,16 +12,16 @@ const { SuccessModel, ErrorModel } = require('../model/resModel')
 router.get('/list', function(req, res, next) {
   let { author = '', keyword = '' } = req.query
 
-  // if (req.query.isadmin) {
-  //   // 管理员界面
-  //   const loginCheckResult = loginCheck(req)
-  //   if (loginCheckResult) {
-  //     // 未登录
-  //     return loginCheckResult
-  //   }
-  //   // 强制查询自己的博客
-  //   author = req.session.username
-  // }
+  if (req.query.isadmin) {
+    // 管理员界面
+    // const loginCheckResult = loginCheck(req)
+    // if (loginCheckResult) {
+    //   // 未登录
+    //   return loginCheckResult
+    // }
+    // 强制查询自己的博客
+    author = req.session.username
+  }
 
   const result = getList(author, keyword)
   return result.then(listData => {
